0effe2701e6f2b6265804dd05b725cdbc7ae1aba
ChenNengHui <chennenghui@huodongxing.com>
2018-07-16 20:13:46 +0800

    no message

---
+++ retrofit/src/main/java/com/rx/retrofit/listener/Error.java
@@ -0,0 +1,17 @@
+package com.rx.retrofit.listener;
+
+/**
+ * Created by ChenNengHui on 2018/7/16.
+ *
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * @@@@ @@@@@@    @@     @@   @@   @@
+ * @@@@ @@@        @@ @   @@   @@   @@
+ * @@@@ @@         @@  @  @@   @@@@@@@
+ * @@@@ @@@        @@   @ @@   @@   @@
+ * @@@@ @@@@@@    @@     @@   @@   @@
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ */
+@FunctionalInterface
+public interface Error {
+    void Error (Object... values);
+}
---
+++ retrofit/src/main/java/com/rx/retrofit/listener/Progress.java
@@ -0,0 +1,17 @@
+package com.rx.retrofit.listener;
+
+/**
+ * Created by ChenNengHui on 2018/7/16.
+ *      下载进度返回
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * @@@@ @@@@@@    @@     @@   @@   @@
+ * @@@@ @@@        @@ @   @@   @@   @@
+ * @@@@ @@         @@  @  @@   @@@@@@@
+ * @@@@ @@@        @@   @ @@   @@   @@
+ * @@@@ @@@@@@    @@     @@   @@   @@
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ */
+@FunctionalInterface
+public interface Progress {
+    void progress(float p);
+}
